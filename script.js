8ed36cddc0808bcb2522c86d8373a53b25332244
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-05-21 09:41:46 -0600

    Refactor chart generation to use async data fetching and improve error handling

--- script.js
+++ script.js
@@ -49,10 +49,19 @@ const despliegaDatos = (datos) => {
 
 document.getElementById("modalButtonGrafica").addEventListener("click", () => {
     console.log("Se da click en el botón de la gráfica");
+    
+    Swal.fire({
+        title: 'Multi App JS',
+        text: "Generando Gráfica ...",
+        icon: "info",
+        showConfirmButton: false,
+        allowOutsideClick: false
+    });
+    
     despliegaGrafica();
 });
 
-const despliegaGrafica = () => {
+const despliegaGrafica = async () => {
     console.log("Se despliega la gráfica");
 
     let chartStatus = Chart.getChart("myChart");
@@ -65,12 +74,10 @@ const despliegaGrafica = () => {
     new Chart(ctx, {
         type: 'bar',
         data: {
-            //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            labels: generaEtiquetas(),
+            labels: await generaEtiquetas(),
             datasets: [{
                 label: '# De Votos',
-                //data: [12, 19, 3, 5, 2, 3],
-                data: generaDatos(),
+                data: await generaDatos(),
                 borderWidth: 1
             }]
         },
@@ -83,20 +90,95 @@ const despliegaGrafica = () => {
         }
     });
 
+    Swal.close();
+
 }
 
-const generaEtiquetas = () => {
+const generaEtiquetas = async () => {
     const etiquetas = [];
-    for (let i = 0; i < 100; i++) {
-        etiquetas.push('Candidato' + i);
+
+    try {
+        await getPersonasData().then((personas) => {
+            console.log('Personas: ', personas);
+            console.log('Cuantas Personas: ', personas.length);
+            personas.forEach((persona) => {
+                console.log('Persona: ', persona.first_name + " " + persona.last_name);
+                etiquetas.push(persona.first_name + " " + persona.last_name);
+            });
+        });
+    
+        return etiquetas;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        Swal.fire({
+            title: "Multi App JS",
+            text: "Error al cargar la información de personas",
+            icon: "error",
+        });
+        return etiquetas;
     }
-    return etiquetas;
+  
 }
 
-const generaDatos = () => {
+const generaDatos = async () => {
     const datos = [];
-    for (let i = 0; i < 100; i++) {
-        datos.push(Math.floor(Math.random() * 100));
+    let numPersonas = 0;
+
+    try {
+        await getPersonasData().then((personas) => {        
+            console.log('Cuantas Personas: ', personas.length);
+            numPersonas = personas.length;
+            for (let i = 0; i < numPersonas; i++) {
+                datos.push(Math.floor(Math.random() * 100));
+            }   
+        });
+       
+        return datos;    
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        Swal.fire({
+            title: "Multi App JS",
+            text: "Error al cargar la información de personas",
+            icon: "error",
+        });
+        return datos;
     }
-    return datos;
+
+}
+
+const getPersonaData = async () => {
+    // Si se necesitan varias personas usar: https://randomuser.me/api/?results=10
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    console.log('datos: ',data);
+    return data.results[0];
+}
+
+const getPersonasData = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("X-API-Key", "2c31e070");
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+
+    try {
+        const response = await fetch("https://my.api.mockaroo.com/clientes_mockaroo.json", requestOptions);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log('datos: ',data);
+        return data;        
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        Swal.fire({
+            title: "Multi App JS",
+            text: "Error al cargar la información de personas",
+            icon: "error",
+        });
+        return [];
+    }
 }
